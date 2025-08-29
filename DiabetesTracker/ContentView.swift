7de1013f9881cb8449af4c49043f9460108ac60d
Megan <megan.wood.cs@gmail.com>
2025-08-29 03:03:46 -0700

    initial commit - popup sheet to add new entry in progress

--- DiabetesTracker/ContentView.swift
+++ DiabetesTracker/ContentView.swift
@@ -10,16 +10,21 @@ import SwiftData
 
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
+    @Query private var entries: [GlucoseEntry]
+    
+    @State private var timestamp: Date = Date.now
+    @State private var selectedType: GlucoseType = .fasting
+    @State private var enteredGlucose: Int = 0
+    @State private var isShowingForm = false
 
     var body: some View {
         NavigationSplitView {
             List {
-                ForEach(items) { item in
+                ForEach(entries) { entry in
                     NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
+                        Text("Item at \(entry.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
                     } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+                        Text(entry.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
                     }
                 }
                 .onDelete(perform: deleteItems)
@@ -29,27 +34,97 @@ struct ContentView: View {
                     EditButton()
                 }
                 ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
+                    Button(action: {
+                        isShowingForm.toggle()
+                        timestamp = Date.now
+
+                    }) {
+//                    Button(action: add Entry) {
+                        Label("Add Entry", systemImage: "plus")
+
+                    }
+                    .sheet(isPresented: $isShowingForm, onDismiss: doDismiss) {
+                        NavigationStack {
+                            VStack {
+                                Form {
+                                    Section() {
+                                        DatePicker(
+                                            "Time and Date: ",
+                                            selection: $timestamp,
+                                            displayedComponents: [.date, .hourAndMinute]
+                                        )
+                                        
+                                    }
+                                }
+                                HStack {
+                                    Button("Cancel", action: {
+                                        isShowingForm.toggle()
+                                        doDismiss()
+                                    })
+                                    .frame(maxWidth: .infinity)
+                                    .padding()
+                                    .background(Color.red.opacity(0.2))
+                                    .foregroundColor(.red)
+                                    .cornerRadius(10)
+                                    
+                                    
+                                    Spacer()
+                                    
+                                    Button("Save") {
+                                        doDismiss()
+                                    }
+                                    .bold()
+                                    .frame(maxWidth: .infinity)
+                                    .padding()
+                                    .background(Color.blue)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(10)
+                                    .buttonStyle(.automatic)
+                                }
+                                .padding([.horizontal, .bottom])
+                            }
+                            .navigationTitle("Add Glucose Entry")
+//                            .toolbar {
+//                                ToolbarItem(placement: .confirmationAction) {
+//                                    Button("Save") {
+//                                        doDismiss()
+//                                    }
+//                                    Button("Cancel", action: {
+//                                        isShowingForm.toggle()
+//                                        doDismiss()
+//                                    })
+//                                }
+//                            }
+                            .padding()
+                            .background(.bar)
+                            
+                        }
                     }
                 }
             }
         } detail: {
             Text("Select an item")
         }
+        
+    }
+    
+    private func doDismiss() {
+        timestamp = Date.now
+        selectedType = .fasting
+        enteredGlucose = 0
     }
 
-    private func addItem() {
+    private func addEntry() {
         withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
+            let newEntry = GlucoseEntry(timestamp: Date(), glucoseValue: enteredGlucose, glucoseType: selectedType)
+            modelContext.insert(newEntry)
         }
     }
 
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
             for index in offsets {
-                modelContext.delete(items[index])
+                modelContext.delete(entries[index])
             }
         }
     }
@@ -57,5 +132,5 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
+        .modelContainer(for: GlucoseEntry.self, inMemory: true)
 }
--- DiabetesTracker/DiabetesTrackerApp.swift
+++ DiabetesTracker/DiabetesTrackerApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct DiabetesTrackerApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            GlucoseEntry.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
